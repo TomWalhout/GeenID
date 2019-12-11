88e1bf7673fb3927950606ba71711108ced84d49
Lordpas3 <Lordpas3@gmail.com>
2019-12-11 12:36:27 +0100

    scaling on program

--- src/objects/Program.ts
+++ src/objects/Program.ts
@@ -1,10 +1,10 @@
 /// <reference path="./GameObject.ts"/>
 
 class Program extends GameObject {
-   
 
-    public constructor(pos: Vector, vel: Vector, ctx: CanvasRenderingContext2D, path: string, frames: number, speed: number) {
-        super(pos, vel, ctx, path, frames, speed)
+
+    public constructor(pos: Vector, vel: Vector, ctx: CanvasRenderingContext2D, path: string, frames: number, speed: number, scale: number) {
+        super(pos, vel, ctx, path, frames, speed, scale)
 
     }
 
