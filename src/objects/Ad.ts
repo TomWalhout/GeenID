4d643d39f43302b78116ba2e8b11e46e22181478
tilb0023 <tilb0023@hz.nl>
2019-12-17 14:15:12 +0100

    Revert "enemy on ad"
    This reverts commit 7c5c7be78a3bebf5098ca734c9dccda80e42087a.

--- src/objects/Ad.ts
+++ src/objects/Ad.ts
@@ -1,7 +1,8 @@
 /// <reference path="./Program.ts"/>
 
 class Ad extends Program {
-    private enemy: Array<Enemy>;
+
+
     private respawn: boolean;
 
     public constructor(pos: Vector, vel: Vector, ctx: CanvasRenderingContext2D, path: string, frames: number, speed: number, scale: number) {
@@ -10,21 +11,11 @@ class Ad extends Program {
         this.open = true;
         this.ctx = ctx;
         this.respawn = false;
-        this.enemy = [];    
     }
 
-    public spawnEnemy(screen : GameScreen) {
-        this.enemy[0] = new Enemy(new Vector(this.randomNumber(100, 100), this.randomNumber(100, 100)), new Vector(0,0), this.ctx, "./assets/Enemy.png", screen, 1, 1);
-        // console.log('bla 2');
-        // console.log(this.enemy[0]);
+    public spawnEnemy() {
     }
 
-    public update() {
-        super.update();
-        if (this.enemy.length > 0) {
-            this.enemy[0].update();
-        }
-    }
 
     public get respawning(): boolean {
         return this.respawn;
@@ -34,13 +25,7 @@ class Ad extends Program {
     public set respawning(v: boolean) {
         this.respawn = v;
     }
-    
-        /**
-     * Renders a random number between min and max
-     * @param {number} min - minimal time
-     * @param {number} max - maximal time
-     */
-    public randomNumber(min: number, max: number): number {
-        return Math.round(Math.random() * (max - min) + min);
-    }
+
+
+
 }
