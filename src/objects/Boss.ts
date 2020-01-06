9d644f39f64b6b46691c3704c2ddce85ef5dcb82
Tom Walhout <twalhout@gmail.com>
2020-01-06 18:10:44 +0100

    If you read this you dumb

--- src/objects/Boss.ts
+++ src/objects/Boss.ts
@@ -8,7 +8,6 @@ class Boss extends GameObject {
     private screen: BossScreen;
     constructor(pos: Vector, vel: Vector, ctx: CanvasRenderingContext2D, path: string, screen: BossScreen, frames: number = 0, speed: number = 0, scale: number = 1) {
         super(pos, vel, ctx, path, frames, speed, scale);
-        this.attackTimer = 0;
         this.ctx = ctx;
         this.screen = screen;
         this.Attack();
@@ -18,32 +17,10 @@ class Boss extends GameObject {
         // vibrate threateningly
         this.vel.x = Math.random() - .5;
         this.vel.y = Math.random() - .5;
-
-        // if (this.currentAttack) {
-        //     this.currentAttack.forEach(element => {
-        //         element.update();
-        //     });
-        // }
         super.update();
     }
 
     private Attack() {
-        // let chance = Math.floor(Math.random() * 5);
-        let chance = 0;
-        switch (chance) {
-            case 0:
-                this.currentAttack = new Array;
-                for (let i = 1; i < 8; i++) {
-                    console.log("apoejrgiajerg");
-                    this.currentAttack[i] = new Codebeam(new Vector(i * 100, 0), new Vector(0, .1 * i * Math.random()), this.ctx);
-                }
-                break;
-            case 1:
-                break;
-            default:
-                console.log("jammerjoh");
-                break;
-        }
     }
 
     public get attack(): GameObject {
