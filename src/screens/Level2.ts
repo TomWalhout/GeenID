02c6bc3bee5a1b7b055cec7b1994173653f831f5
HolyWaffels <41336951+HolyWaffels@users.noreply.github.com>
2020-01-07 11:44:27 +0100

    enemy en ads in level

--- src/screens/Level2.ts
+++ src/screens/Level2.ts
@@ -12,15 +12,20 @@ class Level2 extends LevelScreen {
         // fill this boi up
         this.programs[0] = new Program(new Vector(293, 479), new Vector(0, 0), this.game.ctx, './transparentBreed.png', 1, 1, 1, 0);
         this.programs[0].isOpen = true;
-        this.ads[0] = new Ad (new Vector(this.randomNumber(0, 768), this.randomNumber(0, 1366)), new Vector(0, 0), this.game.ctx, './assets/textboxAndAds/ad1.png', 1, 1, 1)
-        this.ads[0].isOpen = true;
-
+        this.programs[0].hasAds = true;
+        this.ads[0] = new Ad (new Vector(this.randomNumber(100, 1266), this.randomNumber(100, 668)), new Vector(0, 0), this.game.ctx, './assets/textboxAndAds/ad1.png', 1, 1, 1)
+        this.ads[1] = new Ad (new Vector(this.randomNumber(100, 1266), this.randomNumber(100, 668)), new Vector(0, 0), this.game.ctx, './assets/textboxAndAds/ad1.png', 1, 1, 1)
+        this.ads[2] = new Ad (new Vector(this.randomNumber(100, 1266), this.randomNumber(100, 668)), new Vector(0, 0), this.game.ctx, './assets/textboxAndAds/ad1.png', 1, 1, 1)
         document.body.style.backgroundImage = "url('./assets/programs/GloooleLevel.png')";
         }
-
+  
     public draw() {
         super.draw(this.game.ctx);
-        this.ads[0].update();
-        this.closeAds();
+        if (this.ads.length > 0) {
+            this.ads.forEach(e => {
+                e.update();
+            })
+        }
+        this.closeAds()
         }
 }
