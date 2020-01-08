07f2a138e3700ff5b26e85fb020887787344cfdd
Lordpas3 <Lordpas3@gmail.com>
2020-01-08 12:03:52 +0100

    level4update

--- build/app.js
+++ build/app.js
@@ -1125,10 +1125,14 @@ class Level4 extends LevelScreen {
             this.enemies[i] = new Enemy(new Vector(this.randomRoundedNumber(0, this.game.canvas.width - 145), this.randomRoundedNumber(0, this.game.canvas.height - 95)), new Vector(this.randomNumber(0.5, 3), this.randomNumber(0.5, 3)), this.game.ctx, './assets/enemiesAndAllies/Enemy.png', this);
         }
         this.story = 0;
-        this.timeInFrames = 20;
+        this.timeInFrames = 200;
+        this.wizard = new Wizard(new Vector(300, this.game.canvas.height - 145), new Vector(0, 0), this.game.ctx, './assets/enemiesAndAllies/urawizardgandalf.png', 6, 10, 1);
+        this.textbox = new GameObject(new Vector(50, 400), new Vector(0, 0), this.game.ctx, './assets/textboxAndAds/textbox2.png', 1, 1, 1.5);
     }
     draw() {
-        super.draw(this.game.ctx);
+        this.wizard.update();
+        this.textbox.update();
+        this.storyText();
         this.closeAds();
         this.closeProgram();
         this.clickedIcon();
@@ -1139,6 +1143,7 @@ class Level4 extends LevelScreen {
         });
         this.timer();
         this.enemyCollision();
+        super.draw(this.game.ctx);
     }
     timer() {
         if (this.timeInFrames > 0) {
@@ -1149,7 +1154,7 @@ class Level4 extends LevelScreen {
             this.story = 1;
         }
         if (this.story === 1) {
-            this.icons[0] = new Icon(new Vector(this.game.canvas.width - 100, 500), new Vector(0, 0), this.game.ctx, './assets/icons/DEZEPC.png', 1, 1, 1.4);
+            this.icons[0] = new Icon(new Vector(this.game.canvas.width - 100, 500), new Vector(0, 0), this.game.ctx, './assets/icons/virusscanner.png', 1, 1, 0.3);
             let scanner = this.icons[0].box();
             if (this.collides(this.player.box(), scanner)) {
                 this.game.switchScreen(new BossScreen(this.game));
@@ -1161,6 +1166,14 @@ class Level4 extends LevelScreen {
             if (this.collides(this.player.box(), this.enemies[i].box())) {
                 this.id.youGotRekt = this.id.youGotRekt - 1;
             }
+        }
+    }
+    storyText() {
+        if (this.story == 0) {
+            this.text = this.multilineText(this.game.ctx, `${this.game.playerinfo[0]}!\nOntwijk de vijanden terwijl ik\neen virusscanner maak!`, 200, 450);
+        }
+        if (this.story == 1) {
+            this.text = this.multilineText(this.game.ctx, `Goedzo! Ga nu snel\nnaar de scanner toe!`, 200, 450);
         }
     }
 }
